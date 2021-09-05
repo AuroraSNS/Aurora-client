4d4a9ebabf43816be3fc19b9362a9c777df60a62
tjdgus3160 <tjdgus3160@gmail.com>
2021-09-05 16:02:29 +0900

    Fix Oauth redirect url

--- src/components/login/loginForm/googleOAuth/GoogleOAuthPresenter.tsx
+++ src/components/login/loginForm/googleOAuth/GoogleOAuthPresenter.tsx
@@ -4,7 +4,7 @@ import { Wrapper } from './style';
 
 const GoogleOAuthPresenter = () => (
     <Wrapper>
-        <a href="https://api.aurora.center/oauth2/authorization/google?redirect_uri=http://localhost:3000/">
+        <a href="https://api.aurora.center/oauth2/authorization/google?redirect_uri=https://www.aurorasns.com/">
             <IconGoogle />
             <span>구글로 로그인</span>
         </a>
